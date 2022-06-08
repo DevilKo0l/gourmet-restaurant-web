412fb622933e1a317118a4cf59d2505e042fa7fc
DevilKo0l <nguyen.tcao96@gmail.com>
2022-06-08 16:46:58 +0200

    Update header section

--- src/container/Header/Header.jsx
+++ src/container/Header/Header.jsx
@@ -1,10 +1,28 @@
-import React from 'react';
+import React from "react";
 
-import './Header.css';
+import { SubHeading } from "../../components";
+import { images } from "../../constants";
+
+import "./Header.css";
 
 const Header = () => (
-  <div>
-    Header
+  <div className="app__header app__wrapper section__padding" id="home">
+    <div className="app__wrapper_info">
+      <SubHeading title="Chase the new flavour" />
+      <h1 className="app__header-h1">The Key to Fine Dining</h1>
+      <h1 className="p__opensans" style={{ margin: "2rem 0" }}>
+        Sit tellus lobortis sed senectus vivamus molestie. Condimentum volutpat
+        morbi facilisis quam scelerisque sapien. Et, penatibus aliquam amet
+        tellus
+      </h1>
+      <button type="button" className="custom__button">
+        Explore Menu
+      </button>
+    </div>
+
+    <div className="app__wrapper_img">
+      <img src={images.welcome} alt="header img" />
+    </div>
   </div>
 );
 
