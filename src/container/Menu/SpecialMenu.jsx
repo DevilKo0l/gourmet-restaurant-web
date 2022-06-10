85828087fcbddcead1a84507e7f5268f471b0a2d
DevilKo0l <nguyen.tcao96@gmail.com>
2022-06-10 19:06:28 +0200

    Update special menu

--- src/container/Menu/SpecialMenu.jsx
+++ src/container/Menu/SpecialMenu.jsx
@@ -8,12 +8,12 @@ const SpecialMenu = () => (
   <div className="app__specialMenu flex__center section__padding" id="menu">
     <div app="app__specialMenu-title">
       <SubHeading title="Menu that fits your palatte" />
-      <h1 className="headtext-cormorant">Today's Special</h1>
+      <h1 className="headtext__cormorant">Today's Special</h1>
     </div>
 
     <div className="app__specialMenu-menu">
       <div className="app__specialMenu-menu_wine flex__center">
-        <p className="app__specialMenu_menu_heading">Wine & Beer</p>
+        <p className="app__specialMenu-menu_heading">Wine & Beer</p>
         <div className="app__specialMenu-menu_items">
           {data.wines.map((wine, index) => (
             <MenuItem
@@ -31,7 +31,7 @@ const SpecialMenu = () => (
       </div>
 
       <div className="app__specialMenu-menu_cocktails flex__center">
-        <p className="app__specialMenu_menu_heading">Wine & Beer</p>
+        <p className="app__specialMenu-menu_heading">Wine & Beer</p>
         <div className="app__specialMenu-menu_items">
           {data.cocktails.map((cocktail, index) => (
             <MenuItem
