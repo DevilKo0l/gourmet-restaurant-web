dfd5284ad78d825738c54e1a0723300856fc07ae
DevilKo0l <nguyen.tcao96@gmail.com>
2022-06-14 17:38:51 +0200

    Update find us

--- src/container/Findus/FindUs.jsx
+++ src/container/Findus/FindUs.jsx
@@ -1,8 +1,37 @@
-import React from 'react';
+import React from "react";
+
+import { SubHeading } from "../../components";
+import { images } from "../../constants";
 
 const FindUs = () => (
-  <div>
-    FindUs
+  <div className="app__bg app__wrapper section__padding">
+    <div app="app__wrapper_info">
+      <SubHeading title="Contact" />
+      <h1 className="headtext__cormorant" style={{ marginBottom: "3rem" }}>
+        Find Us
+      </h1>
+
+      <div className="app__wrapper-content">
+        <p className="p__opensans">Krysiewicza 7, 61-825 Poznań, Poland</p>
+        <p
+          className="p__cormorant"
+          style={{ color: "#DCCA78", margin: "2rem 0" }}
+        >
+          Opening Hours
+        </p>
+        <p className="p__opensans">Mon - Fri: 10:00 am - 02:00 am</p>
+        <p className="p__opensans">Sat - Sun: 10:00 am - 03:00 am</p>
+      </div>
+
+      <button className="custom__button" style={{ marginTop: "2rem" }}>
+        {" "}
+        Visit Us
+      </button>
+    </div>
+
+    <div className="app__wrapper_img app__wrapper_img-reverse">
+      <img src={images.findus} alt="Find Us" />
+    </div>
   </div>
 );
 
